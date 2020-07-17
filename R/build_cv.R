ca7b981db4345a25ed945661827dd7d0177582de
Michael Schramm <mpschramm@gmail.com>
2020-07-17 13:30:45 -0500

    some refactoring

--- R/build_cv.R
+++ R/build_cv.R
@@ -1,14 +1,16 @@
 #' Build CV
 #'
-#' @param full_name My name
-#' @param data_location character, link to google sheets
-#' @param peer_review Object of class `BibEntry`
-#' @param tech_report Object of class `BibEntry`
-#' @param conference Object of class `BibEntry`
-#' @param software Object of class `BibEntry`
-#' @param datasets Object of class `BibEntry`
-#' @param output_file_name what should the output doc name be
-#' @param output_dir output directory, defaults to current working directory
+#' @param full_name character string, my name.
+#' @param data_location character string, link to google sheets document.
+#' @param peer_review Object of class `BibEntry`.
+#' @param tech_report Object of class `BibEntry`.
+#' @param conference Object of class `BibEntry`.
+#' @param software Object of class `BibEntry`.
+#' @param datasets Object of class `BibEntry`.
+#' @param rmd_template character string indicating path to rmarkdown template or one of `c(svm_cv)`.
+#' @param output_file_name character string for the name of the output document.
+#' @param output_dir character string for the name of the output directory, defaults to current working directory.
+#' @param spell_check logical use the spell_check package to spell check output? defaults to FALSE
 #'
 #' @return files written to the current directory
 #' @export
@@ -16,6 +18,7 @@
 #' @importFrom RefManageR ReadZotero WriteBib
 #' @importFrom rmarkdown render
 #' @importFrom bookdown pdf_document2
+#' @importFrom spelling spell_check_files
 build_cv <- function(full_name = "Michael Schramm",
                      data_location = NULL,
                      peer_review = NULL,
@@ -23,20 +26,24 @@ build_cv <- function(full_name = "Michael Schramm",
                      conference = NULL,
                      software = NULL,
                      datasets = NULL,
+                     rmd_template = c("svm_cv"),
                      output_file_name = "cv.pdf",
-                     output_dir = getwd()) {
+                     output_dir = getwd(),
+                     spell_check = FALSE) {
 
   ## get the rmd file path and other file paths
-  template_loc <- fs::path(system.file("templates/", package = "CVR"), "cv.Rmd")
-  tex_loc <- fs::path(system.file("templates/", package = "CVR"), "svm-latex-cv.tex")
-  lua_mb_loc <- fs::path(system.file("templates/", package = "CVR"), "multiple-bibliographies.lua")
-  csl_loc <- fs::path(system.file("templates/", package = "CVR"), "modapa.csl")
+  if(rmd_template == "svm_cv") {
+
+    template_loc <- fs::path(system.file("templates/", package = "CVR"), "svm-latex-cv.Rmd")
+    tex_loc <- fs::path(system.file("templates/", package = "CVR"), "svm-latex-cv.tex")
+    lua_mb_loc <- fs::path(system.file("templates/", package = "CVR"), "multiple-bibliographies.lua")
+    pandoc_args <- paste0("--lua-filter=", lua_mb_loc)
+    csl_loc <- fs::path(system.file("templates/", package = "CVR"), "modapa.csl")
+
+  }
 
 
   cv_data <- create_CV_object(data_location = data_location)
-
-  ref_data <- RefManageR::ReadZotero(group = "2533336",
-                        .params=list(key='jaGy6K1gbCoBGcq2BWtHsvop'))
 
   peer_review_path <- paste0(output_dir, "/peer_review.bib")
   RefManageR::WriteBib(peer_review, file = peer_review_path)
@@ -53,15 +60,27 @@ build_cv <- function(full_name = "Michael Schramm",
   data_path <- paste0(output_dir, "/datasets.bib")
   RefManageR::WriteBib(datasets, file = data_path)
 
-  pandoc_args = paste0("--lua-filter=", lua_mb_loc)
-  rmarkdown::render(input = template_loc,
-                    bookdown::pdf_document2(latex_engine = "pdflatex",
-                                            template = tex_loc,
-                                            number_sections = FALSE,
-                                            pandoc_args = pandoc_args),
-                    output_file = output_file_name,
-                    output_dir = output_dir,
-                    intermediates_dir = output_dir)
+
+  if (rmd_template == "svm_cv") {
+
+    rmarkdown::render(input = template_loc,
+                      bookdown::pdf_document2(latex_engine = "pdflatex",
+                                              template = tex_loc,
+                                              number_sections = FALSE,
+                                              pandoc_args = pandoc_args),
+                      output_file = output_file_name,
+                      output_dir = output_dir,
+                      intermediates_dir = output_dir)
+  }
+
+  else {
+    rmarkdown::render(output_file = output_file_name,
+                      output_dir = output_dir,
+                      intermediates_dir = output_dir)
+  }
+  if (spell_check) {
+    spelling::spell_check_files(path = paste0(output_dir, output_file_name))
+  }
 
   }
 
--- R/create_cv_object.R
+++ R/create_cv_object.R
@@ -7,58 +7,44 @@
 #'   data types or a path to a folder containing four `.csv`s with the neccesary
 #'   data.
 #' @param source_location Where is the code to build your CV hosted?
-#' @param pdf_mode Is the output being rendered into a pdf? Aka do links need
-#'   to be stripped?
+
 #' @param sheet_is_publicly_readable If you're using google sheets for data,
 #'   is the sheet publicly available? (Makes authorization easier.)
 #' @return A new `CV_Printer` object.
 #' @export
 #' @importFrom stringr str_detect str_extract
 #' @importFrom googlesheets4 gs4_deauth read_sheet
-#' @importFrom readr read_csv
 #' @importFrom lubridate year ymd dmy
 #' @importFrom tidyr unite starts_with
 #' @importFrom dplyr mutate case_when arrange mutate_all desc
 #' @importFrom rlang .data
 create_CV_object <-  function(data_location,
                               source_location = "github.com/mps9506",
-                              pdf_mode = FALSE,
                               sheet_is_publicly_readable = TRUE) {
 
   cv <- list(
-    pdf_mode = pdf_mode,
     links = c()
   )
 
-  is_google_sheets_location <- stringr::str_detect(data_location, "docs\\.google\\.com")
 
-  if(is_google_sheets_location){
-    if(sheet_is_publicly_readable){
-      # This tells google sheets to not try and authenticate. Note that this will only
-      # work if your sheet has sharing set to "anyone with link can view"
-      googlesheets4::gs4_deauth()
-    } else {
-      # My info is in a public sheet so there's no need to do authentication but if you want
-      # to use a private sheet, then this is the way you need to do it.
-      # designate project-specific cache so we can render Rmd without problems
-      options(gargle_oauth_cache = ".secrets")
-    }
-
-    read_gsheet <- function(sheet_id){
-      googlesheets4::read_sheet(data_location, sheet = sheet_id, skip = 1, col_types = "c")
-    }
-    cv$entries_data  <- read_gsheet(sheet_id = "entries")
-    cv$grants  <- read_gsheet(sheet_id = "grants")
-    cv$skills        <- read_gsheet(sheet_id = "language_skills")
-    cv$text_blocks   <- read_gsheet(sheet_id = "text_blocks")
-    cv$contact_info  <- read_gsheet(sheet_id = "contact_info")
+  if(sheet_is_publicly_readable){
+    # This tells google sheets to not try and authenticate. Note that this will only
+    # work if your sheet has sharing set to "anyone with link can view"
+    googlesheets4::gs4_deauth()
   } else {
-    # Want to go old-school with csvs?
-    cv$entries_data <- readr::read_csv(paste0(data_location, "entries.csv"), skip = 1)
-    cv$skills       <- readr::read_csv(paste0(data_location, "language_skills.csv"), skip = 1)
-    cv$text_blocks  <- readr::read_csv(paste0(data_location, "text_blocks.csv"), skip = 1)
-    cv$contact_info <- readr::read_csv(paste0(data_location, "contact_info.csv"), skip = 1)
+    # My info is in a public sheet so there's no need to do authentication but if you want
+    # to use a private sheet, then this is the way you need to do it.
+    # designate project-specific cache so we can render Rmd without problems
+    options(gargle_oauth_cache = ".secrets")
   }
+
+  read_gsheet <- function(sheet_id){
+    googlesheets4::read_sheet(data_location, sheet = sheet_id, skip = 1, col_types = "c")
+  }
+  cv$entries_data  <- read_gsheet(sheet_id = "entries")
+  cv$skills        <- read_gsheet(sheet_id = "language_skills")
+  cv$text_blocks   <- read_gsheet(sheet_id = "text_blocks")
+  cv$contact_info  <- read_gsheet(sheet_id = "contact_info")
 
 
   extract_year <- function(dates){
@@ -109,51 +95,14 @@ create_CV_object <-  function(data_location,
 }
 
 
-#' @title Sanitize Links
-#' @description Remove links from a text block and add to internal list
-#'
-#' @param cv cv object
-#' @param text string
-#'
-#' @return list
-#' @keywords internal
-#' @importFrom stringr str_extract_all str_replace_all
-#' @importFrom purrr set_names
-#' @importFrom rlang .data
-sanitize_links <- function(cv, text){
-  if(cv$pdf_mode){
-    link_titles <- stringr::str_extract_all(.data$text, '(?<=\\[).+?(?=\\])')[[1]]
-    link_destinations <- stringr::str_extract_all(.data$text, '(?<=\\().+?(?=\\))')[[1]]
-
-    n_links <- length(cv$links)
-    n_new_links <- length(link_titles)
-
-    if(n_new_links > 0){
-      # add links to links array
-      cv$links <- c(cv$links, link_destinations)
-
-      # Build map of link destination to superscript
-      link_superscript_mappings <- purrr::set_names(
-        paste0("<sup>", (1:n_new_links) + n_links, "</sup>"),
-        paste0("(", link_destinations, ")")
-      )
-
-      # Replace the link destination and remove square brackets for title
-      text <- text %>%
-        stringr::str_replace_all(stringr::fixed(link_superscript_mappings)) %>%
-        stringr::str_replace_all('\\[(.+?)\\]', "\\1")
-    }
-  }
-
-  list(cv = cv, text = text)
-}
-
-
+#' @title Print Section
 #' @description Take a position data frame and the section id desired and prints the section to markdown.
 #'
 #' @param cv output from create_cv_object
 #' @param glue_template glue string
 #' @param section_id ID of the entries section to be printed as encoded by the `section` column of the `entries` table
+#' @return formatted string
+#' @export
 #' @importFrom dplyr filter
 #' @importFrom glue glue_data
 #' @keywords internal
@@ -173,7 +122,8 @@ print_section <- function(cv, section_id, glue_template = "default"){
   # so links for the same position are right next to each other in number.
   for(i in 1:nrow(section_data)){
     for(col in c('title', 'description_bullets')){
-      strip_res <- sanitize_links(cv, section_data[i, col])
+      #strip_res <- sanitize_links(cv, section_data[i, col])
+      strip_res <- list(cv = cv, text = section_data[i, col])
       section_data[i, col] <- strip_res$text
       cv <- strip_res$cv
     }
@@ -186,15 +136,20 @@ print_section <- function(cv, section_id, glue_template = "default"){
 
 
 
+#' @title Print Text Block
 #' @description Prints out text block identified by a given label.
 #' @param label ID of the text block to print as encoded in `label` column of `text_blocks` table.
+#' @return markdown formatted string
 #' @importFrom dplyr filter pull
 #' @importFrom rlang .data
+#' @export
+#' @keywords internal
 print_text_block <- function(cv, label){
   text_block <- dplyr::filter(cv$text_blocks, .data$loc == label) %>%
     dplyr::pull(.data$text)
 
-  strip_res <- sanitize_links(cv, text_block)
+  #strip_res <- sanitize_links(cv, text_block)
+  strip_res <- list(cv = cv, text = text_block)
 
   cat(strip_res$text)
 
@@ -202,57 +157,13 @@ print_text_block <- function(cv, label){
 }
 
 
-
-#' @description Construct a bar chart of skills
-#' @param out_of The relative maximum for skills. Used to set what a fully filled in skill bar is.
-#' @importFrom dplyr mutate
-#' @importFrom glue glue_data
-#' @importFrom rlang .data
-print_skill_bars <- function(cv, out_of = 5, bar_color = "#969696", bar_background = "#d9d9d9", glue_template = "default"){
-
-  if(glue_template == "default"){
-    glue_template <- "
-<div
-  class = 'skill-bar'
-  style = \"background:linear-gradient(to right,
-                                      {bar_color} {width_percent}%,
-                                      {bar_background} {width_percent}% 100%)\"
->{skill}</div>"
-  }
-  cv$skills %>%
-    dplyr::mutate(width_percent = round(100*as.numeric(.data$level)/out_of)) %>%
-    glue::glue_data(glue_template) %>%
-    print()
-
-  invisible(cv)
-}
-
-
-
-#' @description List of all links in document labeled by their superscript integer.
-#' @importFrom purrr walk2
-#' @importFrom glue glue
-print_links <- function(cv) {
-  n_links <- length(cv$links)
-  if (n_links > 0) {
-    cat("
-Links {data-icon=link}
---------------------------------------------------------------------------------
-<br>
-")
-
-    purrr::walk2(cv$links, 1:n_links, function(link, index) {
-      print(glue::glue('{index}. {link}'))
-    })
-  }
-
-  invisible(cv)
-}
-
-
-
+#' @title Print Contact Info
 #' @description Contact information section with icons
+#' @param cv cv object
+#' @return formatted string
 #' @importFrom glue glue_data
+#' @export
+#' @keywords internal
 print_contact_info <- function(cv){
   glue::glue_data(
     cv$contact_info,
