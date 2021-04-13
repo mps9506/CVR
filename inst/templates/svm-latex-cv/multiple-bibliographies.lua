b7eb5c2972b2ec85bd9820cfa5223cec9a4543fe
Michael Schramm <mpschramm@gmail.com>
2021-04-13 08:55:54 -0500

    updates for new pandoc

--- inst/templates/svm-latex-cv/multiple-bibliographies.lua
+++ inst/templates/svm-latex-cv/multiple-bibliographies.lua
@@ -1,10 +1,12 @@
 --[[
 multiple-bibliographies – create multiple bibliographies
-Copyright © 2018-2020 Albert Krewinkel
+
+Copyright © 2018-2021 Albert Krewinkel
 Modified 19/10/2020 by Mitchell O'Hara-Wild
 Permission to use, copy, modify, and/or distribute this software for any
 purpose with or without fee is hereby granted, provided that the above
 copyright notice and this permission notice appear in all copies.
+
 THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
 WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
 MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
@@ -25,6 +27,10 @@ local doc_meta = pandoc.Meta{}
 
 --- Div used by pandoc-citeproc to insert the bibliography.
 local refs_div = pandoc.Div({}, pandoc.Attr('refs'))
+
+-- Div filled by pandoc-citeproc with properties set according to
+-- the output format and the attributes of cs:bibliography
+local refs_div_with_properties
 
 local supports_quiet_flag = (function ()
   -- We use pandoc instead of pandoc-citeproc starting with pandoc 2.11
@@ -74,8 +80,8 @@ local function resolve_doc_citations (doc)
   -- resolve all citations
   -- doc = run_json_filter(doc, '<<PANDOC_PATH>>/pandoc-citeproc')
   doc = run_citeproc(doc)
-  -- remove catch-all bibliography
-  table.remove(doc.blocks)
+  -- remove catch-all bibliography and keep it for future use
+  refs_div_with_properties = table.remove(doc.blocks)
   -- restore bibliography to original value
   doc.meta.bibliography = orig_bib
   return doc
@@ -101,14 +107,14 @@ local function meta_for_pandoc_citeproc (bibliography)
 end
 
 --- Create a bibliography for a given topic. This acts on all divs whose
--- ID matches "bibliography", and uses the path contained within the div
+-- ID starts with "refs", followed by nothing but underscores and
+-- alphanumeric characters.
 local function create_topic_bibliography (div)
-
-  local is_bib = div.identifier == 'bibliography'
-  if not is_bib then
+  local name = div.identifier:match('^refs([_%w]*)$')
+  local bibfile = name and doc_meta['bibliography' .. name]
+  if not bibfile then
     return nil
   end
-  local bibfile = div.content[1].content[1].text
   local tmp_blocks = {pandoc.Para(all_cites), refs_div}
   local tmp_meta = meta_for_pandoc_citeproc(bibfile)
   local tmp_doc = pandoc.Pandoc(tmp_blocks, tmp_meta)
@@ -116,6 +122,9 @@ local function create_topic_bibliography (div)
   -- First block of the result contains the dummy paragraph, second is
   -- the refs Div filled by pandoc-citeproc.
   div.content = res.blocks[2].content
+  -- Set the classes and attributes as pandoc-citeproc did it on refs_div
+  div.classes = refs_div_with_properties.classes
+  div.attributes = refs_div_with_properties.attributes
   return div
 end
 
